98d74a7d496a68b8cc53535678d5f2f430c341b7
maaz-fast <k258210@nu.edu.pk>
2026-02-21 19:56:00 +0500

    Restored all files and fixed project structure

---
+++ POM_Selenium/PageClasses/ProductClass.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json.Linq;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POM_Selenium
+{
+    public class ProductClass : BaseClass
+    {
+        public void AddProductToCart()
+        {
+            chromeDriver.FindElement(By.Id(LocatorClass.Product1)).Click();
+
+            chromeDriver.FindElement(By.XPath(LocatorClass.CartLink)).Click();
+
+            chromeDriver.FindElement(By.Id(LocatorClass.CheckoutButton)).Click();
+        }
+    }
+}

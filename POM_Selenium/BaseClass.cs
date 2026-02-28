dfd752385df89732305eb80e20daf3824225850e
maaz-fast <k258210@nu.edu.pk>
2026-02-28 15:39:00 +0500

    Changes

--- POM_Selenium/BaseClass.cs
+++ POM_Selenium/BaseClass.cs
@@ -8,53 +8,23 @@ namespace POM_Selenium
     public class BaseClass
     {
         public static ChromeDriver chromeDriver;
-
-        public void DriverConfiguration()
+        public void DriverConfigure()
         {
-            var options = new ChromeOptions();
-            options.AddArgument("--headless");
-            options.AddArgument("--disable-gpu");
-            options.AddArgument("--window-size=1920,1080");
 
-            chromeDriver = new ChromeDriver(options);
+            chromeDriver = new ChromeDriver();
         }
-
         public void OpenBrowserAndURL()
         {
-            string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data.json");
-
-            if (!File.Exists(filePath))
-            {
-                string projectRoot = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..\\..\\"));
-                filePath = Path.Combine(projectRoot, "Data.json");
-
-                if (!File.Exists(filePath))
-                {
-                    projectRoot = Path.GetFullPath(Path.Combine(projectRoot, "..\\"));
-                    filePath = Path.Combine(projectRoot, "Data.json");
-                }
-            }
-
-            if (File.Exists(filePath))
-            {
-                var jsonData = JObject.Parse(File.ReadAllText(filePath));
-                var baseUrl = jsonData["baseUrl"].ToString();
-
-                chromeDriver.Manage().Window.Maximize();
-                chromeDriver.Navigate().GoToUrl(baseUrl);
-            }
-            else
-            {
-                throw new FileNotFoundException($"Data.json file not found! Please check the file at this location: {filePath}");
-            }
+            var jsonData = JObject.Parse(File.ReadAllText(@"C:\Users\Muhammad Maaz\source\repos\POM_Selenium\POM_Selenium\Data.json"));
+            string url = jsonData["baseUrl"].ToString();
+            chromeDriver.Manage().Window.Maximize();
+            chromeDriver.Navigate().GoToUrl(url);
         }
 
         public void CloseBrowser()
         {
-            if (chromeDriver != null)
-            {
-                chromeDriver.Quit(); 
-            }
+            chromeDriver.Close();
+
         }
     }
 }
--- POM_Selenium/TestClass/SwagLabTests.cs
+++ POM_Selenium/TestClass/SwagLabTests.cs
@@ -5,50 +5,52 @@ namespace POM_Selenium
     [TestClass]
     public class SwagLabTests
     {
-        LoginClass loginClass;
-        ProductClass productClass;
-        ShipmentOrderClass shipmentOrderClass;
-
-        [TestInitialize]
-        public void Setup()
-        {
-            loginClass = new LoginClass();
-            productClass = new ProductClass();
-            shipmentOrderClass = new ShipmentOrderClass();
-
-            loginClass.DriverConfiguration();
-            loginClass.OpenBrowserAndURL();
-        }
-
-        [TestCleanup]
-        public void Cleanup()
-        {
-            loginClass.CloseBrowser();
-        }
+        
 
         [TestMethod]
         public void ValidLogin()
         {
+            LoginClass loginClass = new LoginClass();
+            loginClass.DriverConfigure();
+            loginClass.OpenBrowserAndURL();
             loginClass.ValidLogin();
+            loginClass.CloseBrowser();
         }
 
         [TestMethod]
         public void InValidLogin()
         {
+            LoginClass loginClass = new LoginClass();
+            loginClass.DriverConfigure();
+            loginClass.OpenBrowserAndURL();
             loginClass.InValidLogin();
+            loginClass.CloseBrowser();
         }
 
         [TestMethod]
         public void FirstNameError()
         {
+            LoginClass loginClass = new LoginClass();
+            ProductClass productClass = new ProductClass();
+            ShipmentOrderClass shipmentOrderClass = new ShipmentOrderClass();
+
+            loginClass.DriverConfigure();
+            loginClass.OpenBrowserAndURL();
             loginClass.ValidLogin();
             productClass.AddProductToCart();
             shipmentOrderClass.InValidShipmentOrder();
+            loginClass.CloseBrowser();
         }
 
         [TestMethod]
         public void OrderPlacemmet()
         {
+            LoginClass loginClass = new LoginClass();
+            ProductClass productClass = new ProductClass();
+            ShipmentOrderClass shipmentOrderClass = new ShipmentOrderClass();
+
+            loginClass.DriverConfigure();
+            loginClass.OpenBrowserAndURL();
             loginClass.ValidLogin();
             productClass.AddProductToCart();
             shipmentOrderClass.ValidShipmentOrder();
